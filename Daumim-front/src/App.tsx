26d96d249ec43582d0417a1774b2c340a4daf09a
noykimchi <noykimchi2@gmail.com>
2024-08-28 18:29:15 +0300

    fix logo

--- Daumim-front/src/App.tsx
+++ Daumim-front/src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import {MainPage} from "./view/MainPage.tsx";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./context/themeContext";
-import ProductList from "./components/ProductList/ProductList";
 
 const App = () => {
   return (
