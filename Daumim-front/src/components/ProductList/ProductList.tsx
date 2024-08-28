b709caebbe502a4b79e267bec9845b4cad370deb
noykimchi <noykimchi2@gmail.com>
2024-08-28 16:50:50 +0300

    add loading button

--- Daumim-front/src/components/ProductList/ProductList.tsx
+++ Daumim-front/src/components/ProductList/ProductList.tsx
@@ -2,21 +2,41 @@ import { ProductCard } from "./ProductCard/ProductCard";
 import { getClasses } from "./ProductListStyle";
 import { useProducts } from "../../hooks/hooks";
 import { Product } from "../../types/Product";
+import LoadingButton from "@mui/lab/LoadingButton";
+import Alert from "@mui/material/Alert";
+
 
 const ProductList = () => {
   const classes = getClasses();
 
   const { data: products, isLoading, error } = useProducts();
 
-  
-
-  return (
-    <div className={classes.productList}>
-      {products.map((product: Product) => (
-        <ProductCard {...product} />
-      ))}
-    </div>
-  );
+  if (isLoading) {
+    return (
+      <>
+        <LoadingButton
+          size="small"
+          loadingIndicator="Loading…"
+          variant="outlined"
+        >
+          טוענים את המוצרים, רק עוד שנייה!
+        </LoadingButton>
+        ;
+      </>
+    );
+  } else if (error) {
+    <>
+      <Alert severity="error">אופס! לא הצלחנו לטעון את המוצרים, נסה שוב בעוד כמה דקות</Alert>
+    </>;
+  } else {
+    return (
+      <div className={classes.productList}>
+        {products.map((product: Product) => (
+          <ProductCard {...product} />
+        ))}
+      </div>
+    );
+  }
 };
 
 export default ProductList;
